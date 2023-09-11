b8ba474977360629db81f291b4292872bb3f22d1
philbalaz <phil.balaz@gmail.com>
2023-09-11 13:41:41 -0400

    asdf

--- exercice.py
+++ exercice.py
@@ -8,9 +8,8 @@ def dissipated_power(voltage, resistance):
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	dot_product = v1[0] * v2[0] + v1[1] * v2[1] == 0
+	return dot_product
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
